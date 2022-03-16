3680742d944ff58b68bf85efde53ea478a2ceadc
Connor Bond <connorbond@driveworks.co.uk>
2022-03-16 15:41:17 +0000

    Add help text to config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-// Config
+// Update these values to match those of your Server URL, DriveWorks Group Alias,
+// Project Name that you wish to be rendered and the name of the Macro to be triggered.
+
 const config = {
     serverUrl: "",
     groupAlias: "",
